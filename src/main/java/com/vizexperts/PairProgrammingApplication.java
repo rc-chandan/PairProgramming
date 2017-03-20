770f0b2c19ca1900e275895b224a16a79c54b3c0
Chandan Kumar <chandan316@outlook.com>
2017-03-20 13:32:58 +0530

    initial setup

---
+++ src/main/java/com/vizexperts/PairProgrammingApplication.java
@@ -0,0 +1,12 @@
+package com.vizexperts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PairProgrammingApplication.class, args);
+	}
+}
